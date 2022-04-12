da2dfdda9183e8f4c1e6fbc0eff24a3e61c3059c
alexaib <alexaib@outlook.es>
2022-04-12 13:42:46 +0200

    anonymous Runnable migrated to lambda

--- src/org/uem/dam/GestorListaCompra/main/GestorListaCompra.java
+++ src/org/uem/dam/GestorListaCompra/main/GestorListaCompra.java
@@ -2,22 +2,18 @@ package org.uem.dam.GestorListaCompra.main;
 
 import org.uem.dam.GestorListaCompra.control.ListaCompraController;
 import org.uem.dam.GestorListaCompra.model.ListaCompra;
-import org.uem.dam.GestorListaCompra.model.Producto;
 import org.uem.dam.GestorListaCompra.view.ListaCompraWindow;
 
 public class GestorListaCompra {
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ListaCompraWindow window = new ListaCompraWindow();
-                ListaCompraController controller = new ListaCompraController(
-                        window,
-                        new ListaCompra()
-                );
-                window.setController(controller);
-                window.setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> { // Runable es interfaz funcional reemplazable por lambda, no es necesario crear objeto anonimo
+            ListaCompraWindow window = new ListaCompraWindow();
+            ListaCompraController controller = new ListaCompraController(
+                    window,
+                    new ListaCompra()
+            );
+            window.setController(controller);
+            window.setVisible(true);
         });
     }
 }
